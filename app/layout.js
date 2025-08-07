6320909a45ed8d18849c322488cf2971d581d038
flame <flamethrower013@gmail.com>
2025-08-07 20:10:38 +0530

    added vercel analytics

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Analytics } from '@vercel/analytics/next';
 
 
 export const metadata = {
@@ -13,6 +14,7 @@ export default function RootLayout({ children }) {
         className={`min-h-screen  antialiased`}
       >
         {children}
+        <Analytics/>
       </body>
     </html>
   );
